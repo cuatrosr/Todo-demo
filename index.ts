899e39ed9c515f96335e1abacb861f3a6c746311
David Montano <cuatro04sr@gmail.com>
2022-01-21 19:09:22 -0500

    message changed

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Well done boy!");
+  res.send("Well done!");
 });
 app.listen(process.env.PORT || 80, () => {
   console.log("The application is listening on port 3000!");
