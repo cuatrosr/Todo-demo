f99424795478e19718ed9c8a6072591560bcb93e
David Montano <cuatro04sr@gmail.com>
2022-01-21 19:06:38 -0500

    fix: deployment to heroku

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Well done boy!");
 });
-app.listen(80, () => {
+app.listen(process.env.PORT || 80, () => {
   console.log("The application is listening on port 3000!");
 });
